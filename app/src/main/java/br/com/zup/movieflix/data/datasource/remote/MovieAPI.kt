6cf5d704e83aa3c957b065f765d49fcdb43347af
jeehmilena <jessicamilenacorrea@gmail.com>
2022-07-05 12:14:57 -0300

    adição do inicio de consumo de API

---
+++ app/src/main/java/br/com/zup/movieflix/data/datasource/remote/MovieAPI.kt
@@ -0,0 +1,16 @@
+package br.com.zup.movieflix.data.datasource.remote
+
+import br.com.zup.movieflix.data.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieAPI {
+
+    @GET("movie/popular")
+    suspend fun getAllMoviesNetwork(
+        @Query("api_key")
+        apiKey: String = API_KEY,
+        @Query("language")
+        language: String
+    ): MovieResponse
+}
